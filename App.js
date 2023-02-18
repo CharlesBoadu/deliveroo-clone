23999e92a14b86a4628f45dba85c2d3fcf1bea03
Charles Osei Boadu <charles.boadu@stu.ucc.edu.gh>
2023-02-18 16:38:50 +0000

    Adding changes

--- App.js
+++ App.js
@@ -7,6 +7,9 @@ import RestaurantScreen from './screens/RestaurantScreen';
 import { Provider } from 'react-redux';
 import { store } from './store'
 import BasketScreen from './screens/BasketScreen';
+import PreparingOrderScreen from './screens/PreparingOrderScreen';
+import DeliveryScreen from './screens/DeliveryScreen';
+
 
 const Stack = createNativeStackNavigator();
 
@@ -19,6 +22,10 @@ export default function App() {
           <Stack.Screen name="Restaurant" component={RestaurantScreen} />
           <Stack.Screen name="Basket" component={BasketScreen} 
           options={{ presentation: 'modal', headerShown: false }}/>
+          <Stack.Screen name="PreparingOrderScreen" component={PreparingOrderScreen} 
+          options={{ presentation: 'fullScreenModal', headerShown: false}}/>
+          <Stack.Screen name="Delivery" component={DeliveryScreen} 
+          options={{ presentation: 'fullScreenModal', headerShown: false }}/>
         </Stack.Navigator>
       </Provider>
     </NavigationContainer>
